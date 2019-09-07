8984e4a7b5006ec929b79c384179ae2aaeb79bbc
Rodrigo Cericatto <graffiti75@gmail.com>
2019-09-07 01:01:05 -0300

    Create Your First MvRxViewModel.

--- app/src/main/java/br/cericatto/mvrxexample/MainFragment.kt
+++ app/src/main/java/br/cericatto/mvrxexample/MainFragment.kt
@@ -5,19 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import br.cericatto.mvrxexample.R
+import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.withState
 import kotlinx.android.synthetic.main.main_fragment.*
 
-data class HelloWorldState(val title : String = "Hello World!")
+data class HelloWorldState(val title: String = "Hello World!") : MvRxState {
+    val excitedTitle = "$title!"
+}
+
+class HelloWorldViewModel(initialState: HelloWorldState) : MvRxViewModel<HelloWorldState>(initialState)
 
 class MainFragment : Fragment() {
-    private val state = HelloWorldState()
+    private val viewModel = HelloWorldViewModel(HelloWorldState())
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        titleView.text = state.title
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = withState(viewModel) { state ->
+        titleView.text = state.excitedTitle
     }
 }
