b4269f6279a68f778031cd957b32866c001a8715
Rodrigo Cericatto <graffiti75@gmail.com>
2019-09-18 20:20:27 -0300

    Initialize State With Fragment Arguments.

--- app/src/main/java/br/cericatto/mvrxexample/UserProfileFragment.kt
+++ app/src/main/java/br/cericatto/mvrxexample/UserProfileFragment.kt
@@ -1,32 +1,31 @@
 package br.cericatto.mvrxexample
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.airbnb.mvrx.*
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.main_fragment.*
 
-data class UserProfileState(val userId: String) : MvRxState
-
-class UserProfileViewModel(initialState: UserProfileState) : MvRxViewModel<UserProfileState>(initialState) {
-    companion object : MvRxViewModelFactory<UserProfileViewModel, UserProfileState> {
-        override fun initialState(viewModelContext: ViewModelContext) : UserProfileState? {
-            val userId = (viewModelContext as FragmentViewModelContext).fragment<UserProfileFragment>().getUserId()
-            return UserProfileState(userId)
-        }
-    }
+data class UserProfileState(val userId: String) : MvRxState {
+    constructor(args: UserProfileArgs) : this(args.userId)
 }
+
+class UserProfileViewModel(initialState: UserProfileState) : MvRxViewModel<UserProfileState>(initialState)
+
+@Parcelize
+data class UserProfileArgs(val userId: String) : Parcelable
 
 class UserProfileFragment : BaseMvRxFragment() {
 
     private val viewModel : UserProfileViewModel by fragmentViewModel()
+//    private val args: UserProfileArgs by args()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
-
-    fun getUserId() = arguments?.getString(ARG_USER_ID) ?: throw IllegalArgumentException("You must provide a user id.")
 
     override fun invalidate() = withState(viewModel) { state ->
         titleView.text = state.userId
@@ -37,7 +36,7 @@ class UserProfileFragment : BaseMvRxFragment() {
         fun newInstance(userId: String): UserProfileFragment {
             val fragment = UserProfileFragment()
             val args = Bundle()
-            args.putString(ARG_USER_ID, userId)
+            args.putParcelable(MvRx.KEY_ARG, UserProfileArgs(userId))
             fragment.arguments = args
             return fragment
         }
