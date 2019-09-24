35c2ac987b49cef73dd2aa0807074cf9728d2ffa
Rodrigo Cericatto <graffiti75@gmail.com>
2019-09-23 22:08:23 -0300

    MvRx Threading Model.

--- app/src/main/java/br/cericatto/mvrxexample/MainActivity.kt
+++ app/src/main/java/br/cericatto/mvrxexample/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, UserProfileFragment.newInstance("42"))
+                .replace(R.id.container, MainFragment())
                 .commit()
         }
     }
