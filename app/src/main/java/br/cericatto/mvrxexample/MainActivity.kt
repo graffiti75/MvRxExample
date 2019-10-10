270fbf12a986c0f8ae490ee317dee36c79d92d98
Rodrigo Cericatto <graffiti75@gmail.com>
2019-10-10 13:19:05 -0300

    Small fixes.

--- app/src/main/java/br/cericatto/mvrxexample/MainActivity.kt
+++ app/src/main/java/br/cericatto/mvrxexample/MainActivity.kt
@@ -1,9 +1,9 @@
 package br.cericatto.mvrxexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.mvrx.BaseMvRxActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseMvRxActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
